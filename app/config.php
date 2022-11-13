22aa242e5df246a7a876162817b565d68faa16ae
Илья Романенко <ilya13022012@mail.ru>
2022-11-13 21:03:27 +0700

    initial project structure

---
+++ app/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return[
+    'name' => 'Web-interface', // Name application
+    'copyright' => 'Copyright © '. date('Y') . '. Ilya Romanenko.',
+
+    'components' => [ // Include data base
+
+        'host' => 'localhost',  
+        'userName' => 'root',
+        'password' => '',
+        'dataBase' => 'web_interface',
+
+    ],
+];
