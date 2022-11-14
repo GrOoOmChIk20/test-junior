372eef2ef62776f37b91abea5335fa026ea1f900
Илья Романенко <ilya13022012@mail.ru>
2022-11-15 04:54:11 +0700

    add function generatedMatch

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $configApp = include_once './app/config.php';
 
 session_start();
 
-include_once './models//model.php';
+include_once './models/model.php';
 
 $model = new Model($configApp['components']);
 
@@ -19,25 +19,27 @@ $arrTypes = $model->fetchType();
 
 <div class="col-md-6 mx-auto">
     <h4>Add equipment</h4>
+
+    <?php $insert = $model->insert();  ?>
+
     <form action="" method="post">
         <div class="form-group">
-            <textarea class="form-control" placeholder="Serial number (each line has its own serial number)" id="floatingTextarea" name="Equipment[serial_number]"></textarea>
+            <textarea class="form-control" placeholder="Serial number (each line has its own serial number)" id="floatingTextarea" name="Equipment[serials_number]"></textarea>
         </div>
         <div class="form-group">
             <select class="custom-select" id="inputGroupSelect04" aria-label="Example select with button addon" name="Equipment[type]">
                 <option selected>Type of equipment</option>
-                
+
                 <?php
-                
-                    for ($i = 0; $i < count($arrTypes); $i++){
 
-                        $idType = $arrTypes[$i]['id'];
-                        $nameType = $arrTypes[$i]['type'];   
+                for ($i = 0; $i < count($arrTypes); $i++) {
 
-                        echo "<option value='$idType'>$nameType</option>";
-                        
-                    }
-                    
+                    $idType = $arrTypes[$i]['id'];
+                    $nameType = $arrTypes[$i]['type'];
+
+                    echo "<option value='$idType'>$nameType</option>";
+                }
+
                 ?>
 
             </select>
--- models/model.php
+++ models/model.php
@@ -20,11 +20,45 @@ class Model
 
     public function insert()
     {
-<<<<<<< HEAD
+        $equipmentData = $_POST['Equipment'];
 
-=======
-        
->>>>>>> 5f8f1333f13894e509294632c991c6971fc1cd53
+        if (isset($equipmentData['insert'])) {
+
+            $formValidation = $this->validation($equipmentData);
+
+            if ($formValidation['valid']) {
+
+                $equipmentData = $formValidation['validFields'];
+                
+                $fetchMask = $this->fetchMask($equipmentData['type']);
+
+                if ($fetchMask) {
+
+                    $serialsNumber = explode(PHP_EOL, $equipmentData['serials_number']);
+                    $splitMask = str_split($fetchMask);
+
+                    $regularMatch = $this->generatedMatch($splitMask);
+                    // var_dump($serialsNumber);die;
+
+                } else {
+
+                    $_SESSION['errorField'] = 'Type equipment nor found';
+
+                    header("Location: " . $_SERVER["REQUEST_URI"]);
+                    die;
+
+                }
+
+            }else {
+
+                $_SESSION['errorField'] = 'Please fill in all the fields';
+
+                header("Location: " . $_SERVER["REQUEST_URI"]);
+                die;
+
+            }
+
+        }
     }
 
 
@@ -40,21 +74,112 @@ class Model
                 while ($row = mysqli_fetch_assoc($sql)) {
                     $data[] = $row;
                 }
-<<<<<<< HEAD
             }
 
             return $data;
         }
     }
-=======
 
-            } 
+    public function fetchMask($id)
+    {
+        $data = [];
+        $query = "SELECT mask_numb FROM type_equipment WHERE id = $id";
+
+        if ($sql = $this->connect->query($query)) {
+
+            if (mysqli_num_rows($sql) != 0) {
+
+                $data = mysqli_fetch_row($sql)[0];
+
+            }
             
             return $data;
-            
         }
-
     }
 
->>>>>>> 5f8f1333f13894e509294632c991c6971fc1cd53
+    public function validation($data)
+    {
+        $formValid = [];
+        $formValid['valid'] = true;
+        $formValid['validFields'] = [];
+        $formValid['notValidFields'] = [];
+
+        foreach ($data as $key => $value) {
+
+            if ($key == 'insert') continue;
+
+            if (isset($value) && (!empty($value) || $value === '0')) {
+
+                $dataValid = trim($value);
+                $dataValid = stripslashes($value);
+                $dataValid = htmlspecialchars($value);
+
+                $formValid['validFields'][$key] = $dataValid;
+
+            } else {
+
+                $formValid['valid'] = false;
+                $formValid['notValidFields'][] = $key;
+            }
+        }
+
+        return $formValid;
+    }
+
+    public function generatedMatch($mask)
+    {
+        $regularMatch = '';
+
+        for ($i = 0; $i < count($mask); $i++) {
+            
+            if ($i == 0) {
+
+                $regularMatch = '/(^';
+
+                switch ($mask[$i]) {
+                    case 'N':
+                        $regularMatch .= '[0-9].{0,0})';
+                        break;
+                    case 'A':
+                        $regularMatch .= '[A-Z].{0,0})';
+                        break;
+                    case 'a':
+                        $regularMatch .= '[a-z.{0,0}])';
+                        break;
+                    case 'X':
+                        $regularMatch .= '[A-Z-z0-9].{0,0})';
+                        break;
+                    case 'Z':
+                        $regularMatch .= '[-_@.{0,0}])';
+                        break;
+                }
+
+            } else {
+
+                switch ($mask[$i]) {
+                    case 'N':
+                        $regularMatch .= '+([0-9].{0,0})';
+                        break;
+                    case 'A':
+                        $regularMatch .= '+([A-Z].{0,0})';
+                        break;
+                    case 'a':
+                        $regularMatch .= '+([a-z.{0,0}])';
+                        break;
+                    case 'X':
+                        $regularMatch .= '+([A-Z-z0-9].{0,0})';
+                        break;
+                    case 'Z':
+                        $regularMatch .= '+([-_@.{0,0}])';
+                        break;
+                }
+
+            }
+
+        }
+
+        $regularMatch .= '$';
+        
+        return $regularMatch;
+    }
 }
